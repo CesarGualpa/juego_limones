2f02fa8ce1cb66ad651ac4aa5d5d7a39f888cf95
CesarGualpa <cesar_amagu@hotmail.com>
2026-04-16 09:05:57 -0500

    Se agrego detectar piso y mostrar span de vidas

--- limones.js
+++ limones.js
@@ -12,6 +12,8 @@ let personajeX = canvas.width/2;
 let personajeY = canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE);
 let limonX = canvas.width/2;
 let limonY = 0;
+let puntaje = 0;
+let vidas = 3;
 
 
 function iniciar(){
@@ -33,14 +35,14 @@ function dibujarPersonaje(){
 function moverIzquierda(){
     personajeX = personajeX-10;
     actualizarPantalla();
-    detectarColision();
+    detectarAtrapado();
 }
 
 //Colocar mover derecha
 function moverDerecha(){
     personajeX = personajeX+10;
     actualizarPantalla();
-    detectarColision();
+    detectarAtrapado();
 }
 
 //////
@@ -64,23 +66,28 @@ function dibujarLimon(){
 function bajarLimon(){
     limonY = limonY+10;
     actualizarPantalla();
-    detectarColision();
+    detectarAtrapado();
+    detectarPiso();
 }
 
-function detectarColision(){
+function detectarAtrapado(){
     if(limonX+ANCHO_PERSONAJE>personajeX&&
         limonX<personajeX+ANCHO_PERSONAJE&&
         limonY+ALTURA_LIMON>personajeY&&
         limonY<personajeY+ALTURA_PERSONAJE){
         //alert("ATRAPADO!!!!");
         aparecerLimon();
+        puntaje=puntaje+1;
+        mostrarEnSpan("txtPuntaje",puntaje);
     }
 }
 
-function probarAleatorio(){
-    let aleatorio;
-    aleatorio=generarAleatorio(10,80);
-    console.log(aleatorio);
+function detectarPiso(){
+    if(limonY+ALTURA_LIMON==canvas.height-ALTURA_SUELO){
+    aparecerLimon();
+    vidas=vidas-1;
+    mostrarEnSpan("txtVidas",vidas);
+    }
 }
 
 function aparecerLimon(){
